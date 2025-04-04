728b92e478680289a58a4cbf040ee9adf39d6c9d
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-04-04 13:37:24 +0100

    Used Propriedades in Sphere Class

--- BetterColorSpheres/Program.cs
+++ BetterColorSpheres/Program.cs
@@ -36,5 +36,41 @@ namespace ColorSpheres
             return (red + green + blue) / 3;
         }
     }
-    
+    public class Sphere
+    {
+        private readonly Color color;
+        private float radius;
+        private int timesThrown;
+
+        public Sphere(Color color, float radius)
+        {
+            this.color = color;
+            this.radius = radius;
+            this.timesThrown = 0;
+        }
+
+        public void Pop()
+        {
+            radius = 0;
+        }
+
+        public void Throw()
+        {
+            if (radius > 0)
+            {
+                timesThrown++;
+            }
+        }
+
+        public int GetTimesThrown() => timesThrown;
+
+        public void PrintState()
+        {
+            Console.WriteLine($"Color: [R={color.GetRed()}, G={color.GetGreen()}, B={color.GetBlue()}, A={color.GetAlpha()}]");
+            Console.WriteLine($"Radius: {radius}");
+            Console.WriteLine($"Times Thrown: {timesThrown}");
+            Console.WriteLine($"Grey: {color.GetGrey()}");
+        }
+    }
+ 
 }
