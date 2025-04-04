5041563f049a0f94c5b8853af6e5b4ee613d49a4
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-04-04 14:11:30 +0100

    Variaveis de instancia privadas Xp e Health Criadas

---
+++ MyRPG/Player.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Xml.XPath;
+
+namespace MyRPG
+{
+    class Player
+    {
+        private const int xp = 0;
+        private const float health = 100f;
+        private readonly string Name;
+
+        public int xp 
+
+       
+    }
+}
