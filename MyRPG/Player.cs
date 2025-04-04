a0ac5ca43ccbc39afceb3cfbe26b067b5a9543a0
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-04-04 14:38:33 +0100

    My RPG Done

--- MyRPG/Player.cs
+++ MyRPG/Player.cs
@@ -1,16 +1,64 @@
 using System;
-using System.Xml.XPath;
 
 namespace MyRPG
 {
     class Player
     {
-        private const int xp = 0;
-        private const float health = 100f;
-        private readonly string Name;
+        private int xp;
+        private float health;
+        public string Name { get; }
 
-        public int xp 
+        public int XP
+        {
+            get => xp;
+            set
+            {
+                if (value > 0)
+                {
+                    xp += value;
+                }
+            }
+        }
 
-       
+        public int Level
+        {
+            get
+            {
+                return (int)1 + xp / 1000;
+            }
+        }
+
+        public float Health
+        {
+            get
+            {
+                return Math.Clamp(health, 0, MaxHealth);
+            }
+            set
+            {
+                health = Math.Clamp(value, 0, MaxHealth); ;
+            }
+        }
+
+        public float MaxHealth
+        {
+            get
+            {
+                return (float)100 + (Level - 1) * 20;
+            }
+        }
+
+        public void TakeDamage(float damage)
+        {
+            Health -= damage;
+            XP = (int)damage / 20;
+        }
+
+        public Player(string name)
+        {
+            Name = name;
+            xp = 0;
+            Health = MaxHealth;
+        }
     }
 }
